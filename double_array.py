94a7283586eeba6fc952f02ca2340b5a83a28377
fantauzd <fantauzd@oregonstate.edu>
2024-04-15 16:17:42 -0400

    updated name of function foo

--- double_array.py
+++ double_array.py
@@ -1,7 +1,7 @@
 # Dominic Fantauzzo
 
 # Double the value of each element in a given array using divide and conquer (no iteration)
-def foo(arr, start, end):
+def double_array(arr, start, end):
     """
     Doubles the value of each element in the array from start to end.
     :param arr: An array of integers
@@ -12,8 +12,8 @@ def foo(arr, start, end):
     if start < end:
         mid = (start + end) // 2
         # divide the array into two parts
-        foo(arr, start, mid)
-        foo(arr, mid+1, end)
+        double_array(arr, start, mid)
+        double_array(arr, mid+1, end)
     # at the base case, solve the subproblem
     else:
         arr[start] = 2 * arr[start]
@@ -22,7 +22,7 @@ def foo(arr, start, end):
 
 if __name__ == '__main__':
   arr = [1,2,3,4,5,6,7]
-  foo(arr, 0, 6)
+  double_array(arr, 0, 6)
 
   for i in range(0, 7):
     print(arr[i])
