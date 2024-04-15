da29b493863f6e63614bca627f397eee6fa3a190
fantauzd <fantauzd@oregonstate.edu>
2024-04-15 18:58:31 -0400

    fixed error where wrong formula was used to calculate length of left subarray

--- merge_array.py
+++ merge_array.py
@@ -20,16 +20,16 @@ def merge(arr, start, mid, end):
     Merges subarray from start to mid and from mid+1 to end back together in sorted order.
     """
     # temporary arrays to copy elements into subarray
-    left_array = [0] * ((start - mid) + 1)
+    left_array = [0] * ((mid - start) + 1)
     right_array = [0] * (end - mid)
-    for i in range(0, (start - mid) + 1):
+    for i in range(0, (mid - start) + 1):
         left_array[i] = arr[start + i]
     for i in range(0, end - mid):
         right_array[i] = arr[mid+1+i]
 
     i, j, k = 0, 0, start
 
-    while i < (start - mid) + 1 and j < (end - mid):
+    while i < ((mid - start) + 1) and j < (end - mid):
         # fill the original array with the smallest element (n-1 comparisons at worst)
         if left_array[i] < right_array[j]:
             arr[k] = left_array[i]
@@ -39,7 +39,7 @@ def merge(arr, start, mid, end):
             j += 1
         k += 1
 
-    while i < (start - mid) + 1:
+    while i < ((mid - start) + 1):
         arr[k] = left_array[i]
         i += 1
         k += 1
