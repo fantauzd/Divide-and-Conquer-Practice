d89cfc080895f5d631e413ca250a20ce24894d00
fantauzd <fantauzd@oregonstate.edu>
2024-04-15 20:24:13 -0400

    function input set up

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,11 @@
 # Search for an element in a list of sorted numbers using Divide and Conquer Technique.
 # If the element is found return the index of the key, else return -1.
+
+def search(numArr, key):
+    """
+    Search for an element in an array of sorted numbers
+    :param numArr: an array of sorted numbers
+    :param key: number
+    :return: index if key is found, else -1
+    """
+
