83cf017360e7d14e55ec511e2efb773dd437888f
fantauzd <fantauzd@oregonstate.edu>
2024-04-16 03:11:36 -0400

    fixed bug that was causing 1 off error by deducting k by 1 in first call.

--- KthElement.py
+++ KthElement.py
@@ -12,9 +12,10 @@ def kthElement(Arr1, Arr2, k):
     :return: the element at the kth position or an error message if k is out of range
     """
     # ensure that the desired index is valid
-    if k >= Arr1.length() + Arr2.length():
+    if k > len(Arr1) + len(Arr2):
         return "Index out of range"
-    return kth_helper(Arr1, 0, Arr2, 0, k)
+    # use k-1 because we do not want the kth index but the kth position which is at index k-1
+    return kth_helper(Arr1, 0, Arr2, 0, k-1)
 
 def kth_helper(Arr1, i, Arr2, j, k):
     """
@@ -25,19 +26,27 @@ def kth_helper(Arr1, i, Arr2, j, k):
     # when we reach base case, return element at kth position
     if k == 0:
         # if we reached the end of an array, the next element comes from remaining array
-        if j >= Arr2.length():
+        if j >= len(Arr2):
             return Arr1[i]
-        if i >= Arr1.length():
+        if i >= len(Arr1):
             return Arr2[j]
         # otherwise return next smallest element
         if Arr1[i] > Arr2[j]:
             return Arr2[j]
         return Arr1[i]
     # if we reached the end of an array, we examine a subarray of the remaining array
-    if j >= Arr2. length():
+    if j >= len(Arr2):
         return kth_helper(Arr1, i+1, Arr2, j, k-1)
-    if i >= Arr1. length():
+    if i >= len(Arr1):
         return kth_helper(Arr1, i, Arr2, j+1, k-1)
     if Arr1[i] > Arr2[j]:
         return kth_helper(Arr1, i, Arr2, j + 1, k - 1)
     return kth_helper(Arr1, i + 1, Arr2, j, k - 1)
+
+
+if __name__ == '__main__':
+  Arr1 = [1,2,3,5,6]
+  Arr2= [3,4,5,6,7]
+  k= 8
+  print(kthElement(Arr1, Arr2, k))
+
